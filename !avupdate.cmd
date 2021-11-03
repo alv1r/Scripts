9f40f342a7648c5ebcf1d9db290419eb68b703e2
Alexandr <alv1r@ya.ru>
2021-11-03 14:49:25 +0300

    update
    update scripts

--- !avupdate.cmd
+++ !avupdate.cmd
@@ -1,16 +1,23 @@
-rem Scritp for update AV scaning engines
+@echo off
+echo Script for update free AV scaning engines
 rem Depends on wget. https://eternallybored.org/misc/wget/
 rem put in the same folder with this script
 rem and AVZ https://www.z-oleg.com/secur/avz/download.php
 rem extract in avz4 folder near the script
+
+if exist "%~dp0wget.exe" (
+  echo Start downloading
+) else (
+  certutil.exe -urlcache -split -f https://eternallybored.org/misc/wget/1.21.2/32/wget.exe
+)
 del /q "%~dp0cureit.exe"
 wget http://download.geo.drweb.com/pub/drweb/cureit/cureit.exe
 del /q "%~dp0KVRT.exe"
 wget http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe
 del /q "%~dp0ransomhide.exe"
 wget http://softget.net/freeware/projects/RansomHide/ransomhide.exe
-del /q "%~dp0ComboFix.exe"
-wget http://www.forospyware.com/sUBs/ComboFix.exe
+REM del /q "%~dp0ComboFix.exe"
+REM wget http://www.forospyware.com/sUBs/ComboFix.exe
 del /q "%~dp0EmsisoftEmergencyKit.exe"
 wget http://download.emsisoft.com/EmsisoftEmergencyKit.exe
 cd .\avz4
